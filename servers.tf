4203eae3c500175314e696947821ac035b396792
noolivaralakshmi <noolivaralakshmi@gmail.com>
2023-05-05 14:58:02 -0400

    terraform sample change

--- servers.tf
+++ servers.tf
@@ -8,13 +8,13 @@
  // }
 //}
 
-//resource "aws_route53_record" "www" {
-  //zone_id = aws_route53_zone.primary.zone_id
-  //name    = "www.example.com"
-  //type    = "A"
-  //ttl     = 300
-  //records = [aws_eip.lb.public_ip]
-//}
+resource "aws_route53_record" "records" {
+  zone_id = "Z03986262CQPCHNJNZM9L"
+  name    = "${var.components[count.index]}-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instances.private_ip]
+}
 
 resource "aws_instance" "instances" {
   count = length(var.components)
