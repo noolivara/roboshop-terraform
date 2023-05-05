9e84faa7f8ad1f02fad7fb3f7b69711aa297d227
noolivaralakshmi <noolivaralakshmi@gmail.com>
2023-05-05 13:57:48 -0400

    terraform sample change

--- servers.tf
+++ servers.tf
@@ -1,10 +1,3 @@
-data "aws_ami" "centos" {
-  owners = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
-
-
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
